de73afd3974d5da835a178558d3ad1f95a33480d
Diep <n08ni.dieppn@gmail.com>
2022-09-23 08:43:44 +0700

    fix: correct folder name

--- src/ui/ui.module.ts
+++ src/ui/ui.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common'
 import { AuthModule } from './Auth/auth.module'
 import { UsersModule } from './Users/users.module'
-import { InterestsModule } from './interests/interests.module'
-import { FeedsModule } from './feeds/feeds.module'
+import { InterestsModule } from './Interests/interests.module'
+import { FeedsModule } from './Feeds/feeds.module'
 
 @Module({
     imports: [AuthModule, UsersModule, InterestsModule, FeedsModule],
