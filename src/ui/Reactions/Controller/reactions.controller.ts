3de01c407193ce7f069cf621f1263b483995c549
Diep <n08ni.dieppn@gmail.com>
2022-09-28 21:12:33 +0700

    docs: correct swagger api tags feed reaction

--- src/ui/Reactions/Controller/reactions.controller.ts
+++ src/ui/Reactions/Controller/reactions.controller.ts
@@ -1,30 +1,19 @@
-import {
-    Body,
-    Controller,
-    Delete,
-    Get,
-    Post,
-    Req,
-    UseGuards,
-} from '@nestjs/common'
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common'
 import {
     ApiBearerAuth,
-    ApiBody,
     ApiOkResponse,
     ApiOperation,
     ApiTags,
 } from '@nestjs/swagger'
-import { ApiImplicitQuery } from '@nestjs/swagger/dist/decorators/api-implicit-query.decorator'
 import { JwtAuthGuard } from 'src/shared/Guards/jwt.auth.guard'
 import { FeedReaction } from 'src/shared/Schemas/feed-reaction.schema'
-import { UserReactionType } from 'src/shared/Types/types'
 import { CreateFeedReactionDto } from '../Dto/create-feed-reaction.dto'
 import { FeedReactionsService } from '../Service/feed-reaction.service'
 
 @Controller('ui/feeds')
-@ApiTags('Comment APIs')
+@ApiTags('Feed Reaction APIs')
 export class FeedReactionController {
-    constructor(private readonly FeedReactionsService: FeedReactionsService) {}
+    constructor(private readonly feedReactionsService: FeedReactionsService) {}
     @Post('/:id/reactions')
     @UseGuards(JwtAuthGuard)
     @ApiBearerAuth()
@@ -43,6 +32,6 @@ export class FeedReactionController {
             created_by: req.user.userId,
         } as CreateFeedReactionDto
 
-        return this.FeedReactionsService.createReactionByFeedId(createReaction)
+        return this.feedReactionsService.createReactionByFeedId(createReaction)
     }
 }
