28eea301828ec29ef4a82692fa9fdd05eb162908
nam.hoang <nam.hoang@rikai.technology>
2022-09-30 01:16:41 +0700

    feat(feed-reaction): create and delete reaction

--- src/ui/Reactions/reactions.module.ts
+++ src/ui/Reactions/reactions.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import {
+    FeedCommentReaction,
+    FeedCommentReactionSchema,
+} from 'src/shared/Schemas/feed-comment-reaction.schema'
+import {
+    FeedComment,
+    FeedCommentSchema,
+} from 'src/shared/Schemas/feed-comment.schema'
+import {
     FeedReaction,
     FeedReactionSchema,
 } from 'src/shared/Schemas/feed-reaction.schema'
@@ -12,7 +20,12 @@ import { FeedReactionsService } from './Service/feed-reaction.service'
     imports: [
         MongooseModule.forFeature([
             { name: FeedReaction.name, schema: FeedReactionSchema },
+            {
+                name: FeedCommentReaction.name,
+                schema: FeedCommentReactionSchema,
+            },
             { name: Feed.name, schema: FeedSchema },
+            { name: FeedComment.name, schema: FeedCommentSchema },
         ]),
     ],
     controllers: [FeedReactionController],
