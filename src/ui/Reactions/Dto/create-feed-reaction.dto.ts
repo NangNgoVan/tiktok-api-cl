fd8ee3db930c516c92c11372f97d3e179d94957f
nam.hoang <nam.hoang@rikai.technology>
2022-09-30 01:16:41 +0700

    feat(feed-reaction): create and delete reaction

--- src/ui/Reactions/Dto/create-feed-reaction.dto.ts
+++ src/ui/Reactions/Dto/create-feed-reaction.dto.ts
@@ -3,16 +3,9 @@ import { IsString } from 'class-validator'
 import { UserReactionType } from 'src/shared/Types/types'
 
 export class CreateFeedReactionDto {
-    feed_id: string
-
     @ApiProperty({
         enum: UserReactionType,
-        isArray: true,
-        example: [
-            UserReactionType.HEART,
-            UserReactionType.LAUGH,
-            UserReactionType.LIKE,
-        ],
+        example: UserReactionType.HEART,
     })
-    type: UserReactionType
+    type: string
 }
