e55ccc42a836e4265e610fce57134a10262c2bdd
Diep <n08ni.dieppn@gmail.com>
2022-10-03 23:21:14 +0700

    fix: remove unneeded casting

--- src/ui/Resources/Service/resources.service.ts
+++ src/ui/Resources/Service/resources.service.ts
@@ -36,7 +36,7 @@ export class FeedResourcesService {
                     configService.getEnv('AWS_IMAGE_BASE_URL') +
                     '/' +
                     resource.path,
-            } as GetFeedResourceDto
+            }
         })
     }
 }
