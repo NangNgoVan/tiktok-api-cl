873a6d324881af09ba4fc2d8bb2eaae45c9de6c7
Diep <n08ni.dieppn@gmail.com>
2022-10-08 15:33:52 +0700

    feat: remove unused import

--- src/ui/ui.module.ts
+++ src/ui/ui.module.ts
@@ -5,12 +5,8 @@ import { InterestsModule } from './Interests/interests.module'
 import { FeedsModule } from './Feeds/feeds.module'
 import { CommentsModule } from './Comments/comments.module'
 import { RefreshTokenBlacklistMiddleware } from 'src/shared/Middlewares/refresh-token-blacklist-middleware.service'
-import { CacheService } from 'src/shared/Services/cache.service'
-import { AuthenticationService } from 'src/ui/Authentication/Service/authentication.service'
-import { JwtService } from '@nestjs/jwt'
 import { ReactionsModule } from './Reactions/reactions.module'
 import { BookmarksModule } from './Bookmarks/bookmarks.module'
-import { RefreshTokenBlacklistService } from 'src/shared/Services/refresh-token-blacklist.service'
 @Module({
     imports: [
         AuthenticationModule,
